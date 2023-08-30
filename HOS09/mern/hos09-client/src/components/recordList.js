1671b84bd592644a496cf8414780376e93f3a2ec
godwinBime <74328112+godwinBime@users.noreply.github.com>
2023-08-30 20:09:35 +0000

    CRUD successfully implemented

--- HOS09/mern/hos09-client/src/components/recordList.js
+++ HOS09/mern/hos09-client/src/components/recordList.js
@@ -18,11 +18,55 @@ export default function RecordList(){
 
     //this method fetches the records from the database
     useEffect(() => {
-        async response = await fetch(`https://fluffy-yodel-7x9vx9764q52wx77-5050.app.github.dev/record/`);
-        if(!response.ok){
-            const message = `An error occured: ${response.statusText}`;
-            window.alert(message);
-            return;
+        async function getRecords(){
+            const response = await fetch(`https://fluffy-yodel-7x9vx9764q52wx77-5050.app.github.dev/record/`);
+            if(!response.ok){
+                const message = `An error occured: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+            const records = await response.json();
+            setRecords();
         }
-    })
+        getRecords();
+        return;
+    }, [records.length]);
+
+    //This method will delete the records
+    async function deleteRecord(id){
+        await fetch('https://fluffy-yodel-7x9vx9764q52wx77-5050.app.github.dev/record/${id}', {
+            method: "DELETE"
+        });
+
+        const newRecords = records.filter((el) => el._id !== id);
+        setRecords(newRecords);
+    }
+
+    //This function will map out the records on the table
+    function recordList(){
+        return records.map((record) => {
+            return(
+                <Record record={record} deleteRecord={() => deleteRecord(record._id)}
+                key={record._id}/>
+            );
+        });
+    }
+
+    //The following section will display the table with the records
+    return(
+        <div>
+            <h3>Record List</h3>
+            <table className="table table-stripped" style={{marginTop: 20}}>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Level</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{recordList}</tbody>
+            </table>
+        </div>
+    );
 }
